6cb4dda3eda3e8f7621a27a433ea0e8238020426
BamBamChina <aleshina.ea@phystech.edu>
2025-09-27 19:20:03 +0300

    Update Graph T(E_nu).py

--- Graph T(E_nu).py
+++ Graph T(E_nu).py
@@ -2,34 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-G_F = 1.167*10**(-11)  #MeV
-m_e = 9.1*10**(-31) #кг
-tetta_W = 0.491
+G_F = 1.166*10**(-11) #MeV**(-2)
+m_e = 0.511 #MeV
+theta_W = 0.491
+sin_sq_w = 0.235
 
-def g_L(tetta_W):
-    return 1+2*(np.sin(tetta_W))**2
-def g_R(tetta_W):
-    return 2*(np.sin(tetta_W))**2
+def g_L(theta_W):
+    return 1+2*sin_sq_w
+def g_R(theta_W):
+    return 2*sin_sq_w
 
 def cross_section(T, E_nu):
-    a = -E_nu*((g_L(tetta_W))**2)*((1-(T/E_nu))**3)/3
-    b = g_L(tetta_W)*g_R(tetta_W)*m_e*T**2/(2*(E_nu)**2)
-    cross_section = (G_F*m_e)*(T*(g_R(tetta_W)**2)+a-b)/(2*np.pi)
+    a = ((g_L(theta_W))**2)*((1-(T/E_nu))**2)
+    b = g_L(theta_W)*g_R(theta_W)*m_e*T/((E_nu)**2)
+    cross_section = ((G_F**2)*m_e)*((g_R(theta_W)**2)+a-b)/(2*np.pi)
     
     return cross_section
 
-t = np.linspace(0.1, 12, 15) #MeV
-e_nu = np.linspace(0.1, 0.5, 15) #MeV
+t = np.linspace(0.1, 0.3, 5) #MeV
+e_nu = np.linspace(0.1, 0.5, 5) #MeV
+
 
 T, E_nu = np.meshgrid(t, e_nu)
 
+def neutrino_departure_angle(T, E_nu):
+    cos_nda = 2*((E_nu)**2+(m_e)**2-T*m_e+E_nu*(T-m_e))/E_nu
+    return cos_nda
+
+mask = [[0 for i in range(len(e_nu))] for j in range(len(t))]
+
+for i in range(len(t)):
+    for j in range(len(e_nu)):
+        if neutrino_departure_angle(t[i], e_nu[j]) >= -1 and neutrino_departure_angle(t[i], e_nu[j]) <= 1:
+            mask[i][j] = 1
+            mask[i][j] = bool(mask[i][j])
+        else:
+            mask[i][j] = bool(mask[i][j])
+print(mask)
 
 
-#print(cross_section(T, E_nu))
+
 
 plt.figure(figsize=(10, 5))
-sns.heatmap(cross_section(T, E_nu), xticklabels=np.round(e_nu[::], 2), 
-           yticklabels=np.round(t[::-1], 1), cbar_kws={'label': 'Значение поперечного сечения'})
+sns.heatmap(cross_section(T, E_nu), xticklabels=np.round(e_nu[::], 1), 
+           yticklabels=np.round(t[::-1], 2), cbar_kws={'label': 'Значение поперечного сечения'})
 plt.xlabel('E_nu, MeV')
 plt.ylabel('T, MeV')
 plt.show()
